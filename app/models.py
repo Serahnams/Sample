be3ea224de43a3fa20e4fbd8cb9e952c2e4126c3
Serahnams <128810177+Serahnams@users.noreply.github.com>
2024-05-30 03:45:40 +0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    text = db.Column(db.String(200), nullable=False)
+    choices = db.Column(db.PickleType, nullable=False)
+    correct_choice = db.Column(db.Integer, nullable=False)
